79a635c2ffd1b166ac5fbc18053d189e67011cef
JandB <blueseajung@naver.com>
2024-01-19 16:50:22 +0900

    jasypt3

--- src/main/java/msacore/jasypt/JasyptProperty.java
+++ src/main/java/msacore/jasypt/JasyptProperty.java
@@ -3,10 +3,12 @@ package msacore.jasypt;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
 @EnableAutoConfiguration
 @ConfigurationProperties(prefix = "encrypt")
+@Component
 public class JasyptProperty {
     String algolithm;
     int keyObtentionIterations;
